db12832a01d51ce8f6bbebd4f72846e3847f8908
darkthread <coding.mz@gmail.com>
2017-02-11 10:30:41 +0800

    Initial version

---
+++ IP2C.NET/CountryFinder.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace IP2C.Net
+{
+    public class IPCountryFinder
+    {
+        Dictionary<string, string> CountryNames = new Dictionary<string, string>();
+        Dictionary<uint, string> IP2CN = new Dictionary<uint, string>();
+        uint[] IPRanges;
+        public string DupDataInfo = string.Empty;
+
+        public IPCountryFinder(string path)
+        {
+            if (!File.Exists(path)) throw new ArgumentException($"{path} not found!");
+            string dupInfo = null;
+            StringBuilder dupData = new StringBuilder();
+            uint lastRangeEnd = 0;
+            string unknownCode = "--";
+            CountryNames.Add(unknownCode, "Unknown");
+            int count = 0;
+            try
+            {
+                foreach (var line in File.ReadAllLines(path))
+                {
+                    if (line.StartsWith("#")) continue;
+                    try
+                    {
+                        //"87597056","87599103","ripencc","1338422400","ES","ESP","Spain"
+                        var p = line.Split(',').Select(o => o.Trim('"')).ToArray();
+                        var st = uint.Parse(p[0]);
+                        var ed = uint.Parse(p[1]);
+                        var cn = p[4];
+
+                        //range gap found
+                        if (lastRangeEnd > 0 && st > lastRangeEnd)
+                        {
+                            //padding unknown range
+                            IP2CN.Add(lastRangeEnd, unknownCode);
+                            IP2CN.Add(st - 1, unknownCode);
+                            count += 2;
+                        }
+
+                        dupInfo = $"{st}-{ed}-{cn}";
+                        IP2CN.Add(st, cn);
+                        IP2CN.Add(ed, cn);
+                        lastRangeEnd = ed + 1;
+                        if (!CountryNames.ContainsKey(cn))
+                            CountryNames.Add(cn, p[6]);
+                    }
+                    catch (ArgumentException aex)
+                    {
+                        dupData.AppendLine($"Duplicated {dupInfo}: {aex.Message}");
+                    }
+                }
+                IPRanges = IP2CN.Select(o => o.Key).OrderBy(o => o).ToArray();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"CSV parsing error: {ex.Message}");
+            }
+            DupDataInfo = dupData.ToString();
+        }
+        public uint GetIPAddrDec(string ipAddr)
+        {
+            byte[] b = IPAddress.Parse(ipAddr).GetAddressBytes();
+            Array.Reverse(b);
+            return BitConverter.ToUInt32(b, 0);
+        }
+
+        public string GetCountryCode(string ipAddr)
+        {
+            uint ip = GetIPAddrDec(ipAddr);
+            int idx = Array.BinarySearch(IPRanges, ip);
+            if (idx < 0)
+            {
+                int idxNearest = ~idx;
+                if (idxNearest > 0) idxNearest--;
+                idx = idxNearest;
+            }
+            return IP2CN[IPRanges[idx]];
+        }
+
+        public string ConvertCountryCodeToName(string cnCode)
+        {
+            if (CountryNames.ContainsKey(cnCode))
+                return CountryNames[cnCode];
+            return cnCode;
+        }
+
+        public string GetCountryName(string ipAddr)
+        {
+            return ConvertCountryCodeToName(GetCountryCode(ipAddr));
+        }
+    }
+}
---
+++ IP2CTest/UnitTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using IP2C.Net;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IP2CTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LoadPathTest()
+        {
+            var finder = new IPCountryFinder("C:\\NOT_THERE\\IpToCountry.csv");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ApplicationException))]
+        public void ParseTest()
+        {
+            var finder = new IPCountryFinder("IpToCountryBad.csv");
+        }
+        [TestMethod]
+        public void FindTest()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var finder = new IPCountryFinder("IpToCountry.csv");
+            sw.Stop();
+            Assert.IsTrue(sw.ElapsedMilliseconds < 1000);
+            var code = finder.GetCountryCode("168.95.1.1");
+            Assert.AreEqual(code, "TW");
+            var name = finder.GetCountryName("168.95.1.1");
+            Assert.AreEqual(name, "Taiwan; Republic of China (ROC)");
+        }
+
+        [TestMethod]
+        public void ResultCheck()
+        {
+            //Verirfy the result by result from ip2c.org
+            var finder = new IPCountryFinder("IpToCountry.csv");
+            var sw = new Stopwatch();
+            var lines = File.ReadAllLines("TestIPs.txt");
+            sw.Start();
+            foreach (var line in lines)
+            {
+                var p = line.Split(',');
+                var code = finder.GetCountryCode(p[0]);
+                if (string.IsNullOrEmpty(p[1])) p[1] = "--";
+                Assert.AreEqual(code, p[1]);
+            }
+            sw.Stop();
+            Debug.WriteLine($@"Time={sw.ElapsedMilliseconds:n0}ms 
+Count={lines.Count()}
+Average ={(double)sw.ElapsedMilliseconds / lines.Count():n2}ms");
+            
+        }
+        
+        public void GenTestIps()
+        {
+            //Get random IPs from https://www.randomlists.com/ip-addresses
+            var src = @"48.134.176.160
+246.91.15.191
+26.205.18.46";
+            WebClient wc = new WebClient();
+            using (StreamWriter sw = new StreamWriter("TestIp2.txt"))
+            {
+                foreach (var ip in src.Split('\n'))
+                {
+                    var res = wc.DownloadString("http://www.ip2c.org/" + ip);
+                    var p = res.Split(';');
+                    sw.WriteLine($"{ip},{p[1]}");
+                }
+            }
+        }
+
+    }
+}
